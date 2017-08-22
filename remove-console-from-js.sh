eb25dc1e3b945d13834f2c11a460fe7047c8a850
Deepak Jha <deepak125.dk@gmail.com>
2017-08-22 22:22:55 +0530

    Added functionality to pass a directory

--- remove-console-from-js.sh
+++ remove-console-from-js.sh
@@ -6,19 +6,32 @@
 # sed usage example:
 # sed -e 's/SEARCH_STRING/REPLACE_STRING/g' file > file.out
 # have to escape all / (forward slashes) because has special meaning in sed
-SEARCH_STRING="(\/\/)*console.(log|debug|info|warn|error|assert|dir|dirxml|trace|group|groupEnd|time|timeEnd|profile|profileEnd|count)\((.*)\);?"
-REPLACE_STRING=""
+SEARCH_STRING="(\/\/)*console.(log|debug|info|warn)\((.*)\);?"
+REPLACE_STRING="void 0"
 
-if [ ! $# == 1 ]; then
-	echo "Must provide 1 argument (the file to modify)"
-elif [ ! -f $1 ]; then
-	echo "This thing you passed is not a file or cannot be found!"
-else
-	echo "Modifying file!"
-	#sed -E "s/$SEARCH_STRING/$REPLACE_STRING/g" $1 > "$1.tmp"
-	sed -E "/$SEARCH_STRING/ d" $1 > "$1.tmp"
+function removeLogFromFile {
+  echo "Modifying file $1!"
+	sed -E "s/$SEARCH_STRING/$REPLACE_STRING/g" $1 > "$1.tmp"
+	# sed -E "s/$SEARCH_STRING/ d" $1 > "$1.tmp"
 	cp "$1.tmp" $1
 	rm "$1.tmp"
-fi
+}
 
+if [ ! $# == 1 ]; then
+  echo "You Must provide 1 argument (the file to modify)"
+  exit 1
+elif [ ! -f $1 ] && [ ! -d $1 ]; then
+  echo "This thing you passed is not a file or directory"
+  exit 1
+elif [ -f $1 ]; then
+  echo "You passed a file!"
+  removeLogFromFile $1
+elif [ -d $1 ]; then
+  echo "You passed a directory $1!"
+  for i in $(find $1 -name "*.js")
+  do
+    removeLogFromFile $i
+  done
+else echo "Not sure what to do with $i";
+fi
 echo "Complete"
